44922dded283e24e433b160715d397979c245df7
Mikheyev_Denis <mikheydd@mail.ru>
2022-12-26 13:29:03 +0300

    Создание метода

--- Task/Program.cs
+++ Task/Program.cs
@@ -8,6 +8,32 @@
 
 string[] array = {"Hello", "2", "world", ":-"};
 Print(array);
+Console.WriteLine();
+string[] resultArray = GetArrayOfShorterElemets(array, 3);
+Print(resultArray);
+
+string[] GetArrayOfShorterElemets(string[] arr, int maxLenght)
+{
+    int length = arr.Length;
+    string[] tempResult = new string[length];
+    string elem = string.Empty;
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        elem = arr[i];
+        if (elem.Length <= maxLenght)
+        {
+            tempResult[count] = elem;
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        result[i] = tempResult[i];
+    }
+    return result;
+}
 
 
 
